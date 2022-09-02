9a675dbc4732fbeae064ced63b7b5198d85e019c
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2022-09-03 00:15:16 +0530

    new update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import memories from './images/memories.png';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      </Container>
   );
 }
 
